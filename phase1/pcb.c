43a60951c952dc440321791e41ad0c037821cdac
semreh17 <sputo99@gmail.com>
2024-12-10 12:53:11 +0100

    first actual commit

--- phase1/pcb.c
+++ phase1/pcb.c
@@ -17,15 +17,24 @@ void mkEmptyProcQ(struct list_head* head) {
 }
 
 int emptyProcQ(struct list_head* head) {
+    return list_empty(head);
 }
 
 void insertProcQ(struct list_head* head, pcb_t* p) {
+    list_add_tail(p, head);
 }
 
 pcb_t* headProcQ(struct list_head* head) {
+    if (emptyProcQ(head)) return NULL;
+    return container_of(head->next, pcb_t, p_list);
 }
 
 pcb_t* removeProcQ(struct list_head* head) {
+    if (emptyProcQ(head)) return NULL;
+    struct list_head* p = head;
+    list_del(head);
+    // devo passargli il pcb? Oppure il list_head che lo punta, boh
+    return container_of(p, pcb_t, p_list);
 }
 
 pcb_t* outProcQ(struct list_head* head, pcb_t* p) {
