bba0ae541e873829168e5e579451b335cdda3844
semreh17 <sputo99@gmail.com>
2024-12-19 11:14:26 +0100

    aggiustati alcuni metodi e aggiunto il gitignore per la compilazione

--- phase1/pcb.c
+++ phase1/pcb.c
@@ -62,9 +62,9 @@ pcb_t* headProcQ(struct list_head* head) {
 }
 
 pcb_t* removeProcQ(struct list_head* head) {
-    if (emptyProcQ(&head)) return NULL;
-    struct list_head* p = head;
-    list_del(&head);
+    if (emptyProcQ(head)) return NULL;
+    struct list_head* p = head->next;
+    list_del(head->next);
     // devo passargli il pcb? Oppure il list_head che lo punta, boh
     return container_of(p, pcb_t, p_list);
 }
@@ -72,19 +72,15 @@ pcb_t* removeProcQ(struct list_head* head) {
 pcb_t* outProcQ(struct list_head* head, pcb_t* p) {
     int flag=0;
     struct list_head* iter;
-    list_for_each(iter,head) {
-        if(iter == p){
-            flag=1;
-
+    list_for_each(iter, head) {
+        if(iter == &p->p_list){
+            flag = 1;
         }
     }
-    
     if (flag) { 
-        list_del(&p);
+        list_del(&p->p_list);
         return p; 
-        }
-
-    else return NULL;
+        } else return NULL;
 }
 
 int emptyChild(pcb_t* p) {
@@ -97,7 +93,7 @@ int emptyChild(pcb_t* p) {
 
 void insertChild(pcb_t* prnt, pcb_t* p) {
 
-    list_add_tail(&p, &prnt->p_child);         //aggiunge p alla lista p_child di prnt
+    list_add_tail(&p->p_list, &prnt->p_child);         //aggiunge p alla lista p_child di prnt
 
 }
 
@@ -105,10 +101,8 @@ pcb_t* removeChild(pcb_t* p) {
     struct list_head *first_child=p->p_child.next;
      if (list_empty(&p->p_child)) {
         return NULL;                    //se la lista dei children è vuota return NULL
-    }
-    
-    else {
-        list_del(&first_child);         //elimina 
+    } else {
+        list_del(&first_child);         //elimina
     }
 }
 
