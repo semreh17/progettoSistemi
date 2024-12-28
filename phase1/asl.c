17054ac7460abb335bcf78256ef677259ba22fcf
semreh17 <sputo99@gmail.com>
2024-12-28 11:47:56 +0100

    implemented insertBlocked plus minor adjusments

--- phase1/asl.c
+++ phase1/asl.c
@@ -1,26 +1,50 @@
 #include "./headers/asl.h"
 
+#include "headers/pcb.h"
+
 static semd_t semd_table[MAXPROC];
 static struct list_head semdFree_h;
 static struct list_head semd_h;
 
 void initASL() {
     INIT_LIST_HEAD(&semdFree_h);
-
-    
-    for (int i = 0; i < MAXPROC; i++) {              //praticamente la stessa cosa di initPcbs()
+    //praticamente la stessa cosa di initPcbs()
+    for (int i = 0; i < MAXPROC; i++) {
         INIT_LIST_HEAD(&semd_table[i].s_procq);  
         list_add_tail(&semd_table[i].s_link, &semdFree_h);  
     }
-
     INIT_LIST_HEAD(&semd_h);
 }
 
+/*
+ *  setting the semaphore address of the pcb equal to semAdd, since that's
+ *  the semaphore were the pcb is blocked, then adding the pcb to the tail
+ *  of the process queue associated with the semaphore.
+ */
 int insertBlocked(int* semAdd, pcb_t* p) {
-    
+    semd_t actualSem = semd_table[*semAdd];
+    if (emptyProcQ(&actualSem.s_procq)) {
+        if (list_empty(&semdFree_h)) {
+            return TRUE;
+        }
+        
+        semd_t *newSem = container_of(&semdFree_h, semd_t, s_link);
+        list_del(semdFree_h.next);
+
+        // passing the address of the list, so the new sem can be added to the tail
+        // then initializing the fields
+        list_add_tail(&newSem->s_link, &semd_h);
+        newSem->s_key = semAdd;
+        mkEmptyProcQ(&newSem->s_procq);
+
+        p->p_semAdd = semAdd;
+        list_add_tail(&p->p_list, &newSem->s_procq);
+    }
+    return FALSE;
 }
 
 pcb_t* removeBlocked(int* semAdd) {
+
 }
 
 pcb_t* outBlockedPid(int pid) {
