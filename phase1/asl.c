b7af965736e3accee8debabb41a8e9e1ce1e91f7
Cippo-Tappo <mattia04.calzolari@gmail.com>
2024-12-31 10:49:54 +0100

    removeBlocked and some suspicions about insertBlocked

--- phase1/asl.c
+++ phase1/asl.c
@@ -22,13 +22,13 @@ void initASL() {
  *  of the process queue associated with the semaphore.
  */
 int insertBlocked(int* semAdd, pcb_t* p) {
-    semd_t actualSem = semd_table[*semAdd];
-    if (emptyProcQ(&actualSem.s_procq)) {
+    semd_t actualSem = semd_table[*semAdd]; //semd_table è array statico di grandezza maxproc, non capisco come potrebbe funzionare 
+    if (emptyProcQ(&actualSem.s_procq)) {   //mettere come indice *semAdd che probabilmente è una cella di memoria tipo Ax542rD_some_shit_65RtF$
         if (list_empty(&semdFree_h)) {
             return TRUE;
         }
         
-        semd_t *newSem = container_of(&semdFree_h, semd_t, s_link);
+        semd_t *newSem = container_of(&semdFree_h, semd_t, s_link);  //perchè container_of? il newSem è un elemento della lista semdFree_h
         list_del(semdFree_h.next);
 
         // passing the address of the list, so the new sem can be added to the tail
@@ -43,8 +43,36 @@ int insertBlocked(int* semAdd, pcb_t* p) {
     return FALSE;
 }
 
+//perdona il miscuglio di engl ed ita ma facevo copia e incolla dalle spec
 pcb_t* removeBlocked(int* semAdd) {
+    semd_t *this_sem;
 
+    list_for_each_entry(this_sem, &semd_h, s_link) {           // search the ASL for a descriptor of this semaphore.
+        if (this_sem->s_key == semAdd) {                       // questo pezzo serve per cercare un semaforo puntato da semAdd
+            break;                                             // nella ASL, fate copia ed incolla se e quando servirà per altro codice 
+        }
+    }
+    
+    if (this_sem->s_key != semAdd) {                            // se il semaforo non è trovato, restituisce NULL
+        return NULL;
+    }
+
+    struct list_head *first = this_sem->s_procq.next;    
+    
+    list_del(first);                                           // rimuove il nodo dalla lista
+
+    pcb_t *removedPcb = container_of(first, pcb_t, p_list);
+
+    
+    if (list_empty(&this_sem->s_procq)) {  
+                                                            
+        list_del(&this_sem->s_link);                        // Se la porcq del semaforo è vuota ( cioè abbiamo eliminato l'ultimo pcb)
+                                                            // remove the semaphore descriptor from the ASL
+        list_add(&this_sem->s_link, &semdFree_h);           // and return it to the semdFree list.
+    }
+
+    removedPcb->p_semAdd = NULL;                           //"cancella" il puntatore al semaforo in cui era precedentemente
+    return removedPcb;
 }
 
 pcb_t* outBlockedPid(int pid) {
