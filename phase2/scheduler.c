bae55d7e2150b95494159f22dffdcf2c5b949d85
Cippo-Tappo <mattia04.calzolari@gmail.com>
2025-03-27 16:31:26 +0100

    aggiunto le macro AQUIRELOCK e RELEASE

--- phase2/scheduler.c
+++ phase2/scheduler.c
@@ -4,11 +4,14 @@ extern int processCount;
 extern int globalLock;            
 extern struct list_head readyQueue;
 
+
+
+
 void scheduler() {
     int core_id = getCPUID(); //robaccia di riscv
     
-    while(globalLock);  // busy wiat per il lock
-    globalLock = 1;
+    
+    ACQUIRE_LOCK();
 
     pcb_t *new_process = removeProcQ(&readyQueue);  //rimuove processo dalla queue
     
@@ -17,37 +20,40 @@ void scheduler() {
         currentProcess[core_id] = new_process;      //assegnazione al core corrente
         
         
+        new_process->p_s.irt = 0;       //irt=interrupt resiaìdual time
+        
         setPLT(TIMESLICE * (*((cpu_t *)TIMESCALEADDR))); //timer che non ho ancora capito come funzioni
         
         
-        globalLock = 0;
+        RELEASE_LOCK();
         
         LDST(&(new_process->p_s));  //caricamento process state
 
     } else {
         
         if(processCount == 0) {     //se la coda è vuota
-            globalLock = 0;
+            RELEASE_LOCK();
             HALT();
         }
 
         else if(processCount > 0 && soft_blocked_count > 0) {
             
-            setSTATUS(getSTATUS() | IECON | IMON);
-            setMIE(getMIE() & ~0x80); //disabilita timer interrupt
+            
+            setTPR(1);      //tpr=task priority manager
+            
+            setMIE(MIE_ALL & ~MIE_MTIE_MASK);
+            unsigned int status = getSTATUS();
+            status |= MSTATUS_MIE_MASK;
+            setSTATUS(status);  //disabilita timer interrupt
             
             currentProcess[core_id] = NULL;
-            globalLock = 0;
+            RELEASE_LOCK();
             WAIT();
         }
 
         else {
-            
-            globalLock = 0;
+            RELEASE_LOCK();
             PANIC();
         }
     }
 }
-
-
-
