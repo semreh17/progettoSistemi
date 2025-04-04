aebcee196152cb984ed2308b0bea518e2be19872
semreh17 <sputo99@gmail.com>
2025-04-04 12:11:13 +0200

    scheduler inshallah zio pera

--- phase2/scheduler.c
+++ phase2/scheduler.c
@@ -1,54 +1,63 @@
-#include "../headers/const.h"
 #include "../headers/types.h"
+#include "../headers/listx.h"
+#include "../headers/const.h"
 #include "../phase1/headers/pcb.h"
+#include "uriscv/liburiscv.h"
 
-extern int processCount;         
-extern int globalLock;
-extern struct pcb_t *currentProcess[NCPU];         
+extern int processCount;
+extern volatile unsigned int globalLock;
 extern struct list_head readyQueue;
+extern struct pcb_t *currentProcess[NCPU];
 
 void scheduler() {
-    int core_id = getPRID(); //robaccia di riscv
-    
-    ACQUIRE_LOCK(&globalLock);
+    unsigned int coreId = getPRID();
 
-    pcb_t *new_process = removeProcQ(&readyQueue);  //rimuove processo dalla queue
-    
-    if(new_process != NULL) {
-        
-        currentProcess[core_id] = new_process;      //assegnazione al core corrente
-        
-        setPLT(TIMESLICE * (*((cpu_t *)TIMESCALEADDR))); //timer che non ho ancora capito come funzioni
-        
-        RELEASE_LOCK();
-        
-        LDST(&(new_process->p_s));  //caricamento process state
+    if (!globalLock) {
+        ACQUIRE_LOCK(&globalLock);
+    }
 
-    } else {
-        
-        if(processCount == 0) {     //se la coda è vuota
-            RELEASE_LOCK();
+    pcb_t *newProcess = removeProcQ(&readyQueue);  //rimuove processo dalla queue
+
+    if (!emptyProcQ(&readyQueue)) {
+        // qua probably manca della roba
+        currentProcess[coreId] = newProcess;      //assegnazione al core corrente
+        RELEASE_LOCK(&globalLock);
+        LDST(&(newProcess->p_s));  //caricamento process state
+    }else {
+        if (processCount == 0) {     //se la coda è vuota
+            RELEASE_LOCK(&globalLock);
             HALT();
-        }
+        }else if (processCount > 0 && coreId != 0) { // non ho capito perché controlli anche il coreId
 
-        else if(processCount > 0 && core_id!=0) {
-            
-            setTPR(1);      //tpr=task priority manager
-            
+            *((memaddr *)TPR) = 1; // setting the TPR to 1
             setMIE(MIE_ALL & ~MIE_MTIE_MASK);
             unsigned int status = getSTATUS();
             status |= MSTATUS_MIE_MASK;
-            setSTATUS(status);  //disabilita timer interrupt
-            
-            currentProcess[core_id] = NULL;
-            RELEASE_LOCK();
+            setSTATUS(status);  // disabilita timer interrupt
+
+            //currentProcess[coreId] = NULL; la commento, ma non so in realtà a cosa serva
+            RELEASE_LOCK(&globalLock);
             WAIT();
         }
-
-        else {
-
-            RELEASE_LOCK();
-            PANIC();
-        }
     }
+}
+#include "../headers/types.h"
+#include "../headers/listx.h"
+#include "../headers/const.h"
+#include "../phase1/headers/pcb.h"
+#include "uriscv/liburiscv.h"
+
+void exceptionHandler() {}
+
+int createProcess(state_t *statep, pcb_t *caller) {
+    pcb_t *newProcess = allocPcb();
+    if (newProcess == NULL) {
+        statep->gpr[24] = -1;
+        return NULL;
+    }
+
+    newProcess->p_s = * (state_t*)statep->gpr[25];
+    newProcess->p_supportStruct = (support_t*)statep->gpr[26];
+    newProcess->p_pid = caller->p_pid++; // uh?
+
 }
